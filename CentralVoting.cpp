16d93e535bb6b9e3ca5b4b7e39bb47ead8a11329
yyh <913217005@qq.com>
2022-07-12 12:12:49 +0800

    完成点云框选

--- CentralVoting.cpp
+++ CentralVoting.cpp
@@ -30,6 +30,44 @@ bool CentralVoting::CenterExtractor(int index) {
 
     view.setBackgroundColor(0, 0, 0);
 
+    pcl::MomentOfInertiaEstimation<pcl::PointXYZ> feature_extractor;
+    feature_extractor.setInputCloud(this->model_set[index]);
+    feature_extractor.compute();
+
+    std::vector<float> moment_of_inertia;
+    std::vector<float> eccentricity;
+    pcl::PointXYZ min_point_AABB;
+    pcl::PointXYZ max_point_AABB;
+    float major_value, middle_value, minor_value;
+    Eigen::Vector3f major_vector, middle_vector, minor_vector;
+    Eigen::Vector3f mass_center;
+    feature_extractor.getAABB(min_point_AABB, max_point_AABB);
+    feature_extractor.getEigenVectors(major_vector, middle_vector,
+                                      minor_vector);
+    feature_extractor.getMassCenter(mass_center);
+    view.addCube(min_point_AABB.x, max_point_AABB.x, min_point_AABB.y,
+                 max_point_AABB.y, min_point_AABB.z, max_point_AABB.z, 1.0, 1.0,
+                 0.0, "AABB");
+    view.setShapeRenderingProperties(
+        pcl::visualization::PCL_VISUALIZER_REPRESENTATION,
+        pcl::visualization::PCL_VISUALIZER_REPRESENTATION_WIREFRAME, "AABB");
+    pcl::PointXYZ center_(mass_center(0), mass_center(1), mass_center(2));
+    pcl::PointXYZ x_axis(major_vector(0) * 100 + mass_center(0),
+                         major_vector(1) * 100 + mass_center(1),
+                         major_vector(2) * 100 + mass_center(2));
+    pcl::PointXYZ y_axis(middle_vector(0) * 100 + mass_center(0),
+                         middle_vector(1) * 100 + mass_center(1),
+                         middle_vector(2) * 100 + mass_center(2));
+    pcl::PointXYZ z_axis(minor_vector(0) * 100 + mass_center(0),
+                         minor_vector(1) * 100 + mass_center(1),
+                         minor_vector(2) * 100 + mass_center(2));
+    view.addLine(center_, x_axis, 1.0f, 0.0f, 0.0f, "major eigen vector");
+    view.addLine(center_, y_axis, 0.0f, 1.0f, 0.0f, "middle eigen vector");
+    view.addLine(center_, z_axis, 0.0f, 0.0f, 1.0f, "minor eigen vector");
+
+    view.addCoordinateSystem(100, mass_center(0), mass_center(1),
+                             mass_center(2));
+
     while (!view.wasStopped()) {
       view.spinOnce(100);
       boost::this_thread::sleep(boost::posix_time::microseconds(1000));
--- CentralVoting.h
+++ CentralVoting.h
@@ -23,7 +23,22 @@
 #include "pcl/point_representation.h"
 #include "pcl/registration/registration.h"
 #include "pcl/visualization/cloud_viewer.h"
-
+#include <limits>
+#include <fstream>
+#include <vector>
+#include <Eigen/Core>
+#include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/features/fpfh.h>
+#include <pcl/registration/ia_ransac.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/search/impl/search.hpp>
+#include <pcl/features/moment_of_inertia_estimation.h>
 #include "Eigen/Core"
 
 class CentralVoting {
